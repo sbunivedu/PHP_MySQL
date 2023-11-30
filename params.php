bdd04c63433a4b4eced31be864903cea356918f4
Baochuan Lu <lubaochuan@gmail.com>
2023-11-30 15:32:44 -0600

    udpate params.php

--- params.php
+++ params.php
@@ -2,8 +2,8 @@
 $first_name = $_GET["firstname"];
 $last_name = $_GET["lastname"];
 
-print("first name: ".$first_name);
-print("last name: ".$last_name);
+print("first name: ".$first_name."\n");
+print("last name: ".$last_name."\n");
 
 print_r($_GET);
 ?>

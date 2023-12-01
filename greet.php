c48a417ecbe02fec1a43ed6b18626ff527a9e179
Baochuan Lu <lubaochuan@gmail.com>
2023-12-01 08:32:05 -0600

    update greet.php

--- greet.php
+++ greet.php
@@ -1,11 +1,11 @@
 <?php
-function greet($name) {
+function greet($name){
   echo "Hello ".$name."!";
 }
 
-function greet1($name) {
+function greet1($name){
 ?>
-  <h1>Hello <?=$name?></h1>
+  <h1>Hello <?=$name?>!</h1>
 <?php
 }
 
@@ -16,4 +16,19 @@ greet("Alice");
 greet1("John");
 greet1("Bob");
 greet1("Alice");
+
+function present_form(){
 ?>
+<form action="form1.php" method="POST" >
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="firstname"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lastname">
+	<input type="submit" value="Submit">
+</form>
+<?php
+}
+?>
+
+present_form();
+present_form();

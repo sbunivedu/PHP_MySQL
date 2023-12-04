45c9d2076ae578395f8361cbae8feaffc37ee525
Baochuan Lu <lubaochuan@gmail.com>
2023-12-04 09:31:56 -0600

    fix include bug

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-include(top.html);
+include("top.html");
 ?>
 
 <h1>Welcome! Bienvenue! Willkommen! 欢迎! ¡Bienvenido! Добро пожаловать!</h1>
 
 <?php
-include(bottom.html);
+include("bottom.html");
 ?>
